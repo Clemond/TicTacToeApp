137bd2b290ffd5e4437ca57f14a8c8953a567dc0
Clemond <quackshows11@gmail.com>
2024-09-05 11:36:48 +0200

    Added labels for the players, the playboard and tags to each square

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
+    
+    @IBAction func onTapGesture(_ sender: UITapGestureRecognizer) {
+        
+        if let view = sender.view {
+            print("You pressed square:\(view.tag)")
+        }
+        
+        
+        
+    }
+    
 }
 
